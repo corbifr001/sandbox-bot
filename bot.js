78dd8ee98cdfdb14f23f87a85002bd1c64a7667e
Francis Corbin <30697770+corbifr001@users.noreply.github.com>
2018-09-01 22:12:18 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,6 +9,23 @@ client.on('message', message => {
     if (message.content === 'asshole') {
     	message.reply('Right back at ya!!');
   	}
+    
+      // If the message is "how to embed"
+  if (message.content === 'how to embed') {
+    // We can create embeds using the MessageEmbed constructor
+    // Read more about all that you can do with the constructor
+    // over at https://discord.js.org/#/docs/main/stable/class/RichEmbed
+    const embed = new RichEmbed()
+      // Set the title of the field
+      .setTitle('A slick little embed')
+      // Set the color of the embed
+      .setColor(0xFF0000)
+      // Set the main content of the embed
+      .setDescription('Hello, this is a slick embed!');
+    // Send the embed to the same channel as the message
+    message.channel.send(embed);
+  }
+
 });
 
 // THIS  MUST  BE  THIS  WAY
