be6e670a7e89491b7240a42d1a921f145dca0d6f
Francis Corbin <30697770+corbifr001@users.noreply.github.com>
2018-09-04 09:41:25 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,23 +12,26 @@ client.on('message', async message => {
     // MAke sure bots can't call bots...
     if(message.author.bot) return;
     
-    if (message.content === 'asshole') {
+    const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
+    
+    if (command === 'asshole') {
     	message.reply('Right back at ya!!');
   	}
     
   // If the message is "what is my avatar"
-  if (message.content === 'what is my avatar') {
+  if (command === 'what is my avatar') {
     // Send the user's avatar URL
     message.reply(message.author.avatarURL);
   }
     
-  if (message.content === 'created') {
+  if (command === 'created') {
     //message.channel.send(message.channel.CreatedAt);
      message.reply('Response: '+message.channel.CreatedAt);
      console.log('Message created at: '+message.channel.CreatedAt);
   }
     
-  if (message.content === "ping") {
+  if (command === "ping") {
     // Calculates ping between sending a message and editing it, giving a nice round-trip latency.
     // The second ping is an average latency between the bot and the websocket server (one-way, not round-trip)
     const m = await message.channel.send("Ping?");
@@ -36,14 +39,30 @@ client.on('message', async message => {
     m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
   }
     
-  if (message.content === "admin") {
+  if (command === "admin") {
       
       
       if(!message.member.roles.some(r=>["Administrator", "Moderator"].includes(r.name)) ) {
         return message.reply("Sorry, you are not an admin!");
       } else {
-          return message.reply("You are an admin");
+          return message.reply("You are an admin or moderator.");
       }
+  }
+    
+  if(command === "purge") {
+    // This command removes all messages from all users in the channel, up to 100.
+    
+    // get the delete count, as an actual number.
+    const deleteCount = parseInt(args[0], 10);
+    
+    // Ooooh nice, combined conditions. <3
+    if(!deleteCount || deleteCount < 2 || deleteCount > 100)
+      return message.reply("Please provide a number between 2 and 100 for the number of messages to delete");
+    
+    // So we get our messages, and delete them. Simple enough, right?
+    const fetched = await message.channel.fetchMessages({limit: deleteCount});
+    message.channel.bulkDelete(fetched)
+      .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
   }
 
 });
