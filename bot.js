feb2adf3707cd513ba1b030745d1e7f7fdbfa984
Francis Corbin <30697770+corbifr001@users.noreply.github.com>
2018-09-05 06:50:52 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -66,7 +66,7 @@ client.on('message', async message => {
   }
     
     if (command === 'testJoin') {
-        message.reply('Testing the GuildMemberAdd method.');
+        return message.reply('Testing the GuildMemberAdd method.');
         //client.emit("guildMemberAdd", message.member);
     }
 
