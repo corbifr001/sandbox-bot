60147a016abcf2ec84fe85b243068b3f2e54e814
Francis Corbin <30697770+corbifr001@users.noreply.github.com>
2018-09-04 22:35:51 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -70,7 +70,7 @@ client.on('message', async message => {
 client.on('guildMemberAdd', GuildMember => {
     //defaultChannel.send('A new member joined. Say hi!');
     
-    member.guild.channels.get("welcome").send(`"${member.user.username}" has joined this server`);
+    member.guild.channels.get("general").send(`"${member.user.username}" has joined this server`);
 });
 
 // Get the Token from the server configuration
