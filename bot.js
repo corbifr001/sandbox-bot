79c773ee5376b99bfc6bf56cc55cbaa7ec217f81
Francis Corbin <30697770+corbifr001@users.noreply.github.com>
2018-09-09 16:54:52 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ const client = new Discord.Client();
 const invites = {};
 const wait = require('util').promisify(setTimeout);
 
-client.on('ready', () => {
+client.on('ready', async () => {
     console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`);
     //client.user.setActivity(`Serving ${client.guilds.size} servers`);
     //console.log('I am ready!');
